e49921d99c225f68caf54c3f3b5cc775619a8c16
Kevin Lamontagne <kevin@demarque.com>
2014-08-18 14:43:27 -0400

    Add license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'google_auth_proxy'
 maintainer       'De Marque Inc.'
 maintainer_email 'kevin@demarque.com'
-license          ''
+license 'MIT'
 description      'Installs/Configures google_auth_proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.1'
