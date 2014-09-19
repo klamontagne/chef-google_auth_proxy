d97a666fa7511b8ee6a16ea9786f808bb867809c
James Awesome <james.morelli@crowdsurge.com>
2014-09-19 17:25:35 -0400

    Add tests for installing google_auth_proxy from url
    - Add install::remote_s3 test to fixtures in order to test installing
      google_auth_proxy binary from a url
    - Patch google_auth_proxy::default to correctly handle the remote_file
      resource and fail properly with an invalid url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,2 @@
 default[:google_auth_proxy][:source] = 'github.com/bitly/google_auth_proxy'
+default[:google_auth_procy][:checksum] = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@
 #
 
 source_address = node[:google_auth_proxy][:source]
+bin_checksum = node[:google_auth_proxy][:checksum]
 
 # Install the google_auth_proxy binary ...
 if (source_address =~ /^github\.com\/.*/)
@@ -11,12 +12,15 @@ if (source_address =~ /^github\.com\/.*/)
   golang_package source_address
 elsif (source_address =~ /^http:|^https:|^file:/)
   Chef::Log.info("Installing 'google_auth_proxy' via remote_file from: #{source_address}")
-  remote_file source_address do
-    path "#{node[:go][:gobin]/google_auth_proxy}"
+  remote_file "#{node[:go][:gobin]}/google_auth_proxy" do
+    source source_address
     owner node[:go][:owner]
     group node[:go][:group]
     mode '0755'
+    if bin_checksum
+      checksum bin_checksum
+    end
   end
 else
-  Chef::Log.fatal("Invalid source for 'google_auth_proxy': #{source_address}")
+  Chef::Application.fatal!("Invalid source for 'google_auth_proxy': #{source_address}")
 end
