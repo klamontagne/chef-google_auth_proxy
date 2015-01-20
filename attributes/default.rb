f9ffe4027a3803f1e8efed67316ef5408f06f21f
Kevin Lamontagne <kevin@demarque.com>
2015-01-20 09:45:24 -0500

    restart(stop+start) service on config or program changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,3 +7,5 @@ default['google_auth_proxy']['bin_path'] = '/usr/bin'
 default['google_auth_proxy']['binary_checksum'] = '2928de1b7a4f6ad6dc119e44ecdef14032de3a454baa25dd342aa257e8ea7ae9'
 default['google_auth_proxy']['binary_url'] = 'https://github.com/bitly/google_auth_proxy/releases/download/v1.0/google_auth_proxy-1.0.linux-amd64.go1.3.tar.gz'
 default['google_auth_proxy']['source_golange_package'] = 'github.com/bitly/google_auth_proxy'
+
+default['google_auth_proxy']['auto_restart'] = true
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kevin@demarque.com'
 license 'MIT'
 description 'Installs/Configures google_auth_proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.0'
+version '2.1.0'
 
 supports 'ubuntu', '>= 12.04'
 
